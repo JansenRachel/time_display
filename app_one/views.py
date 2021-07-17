4c0e771ac609709e45b5eca4a0ca096d076d2955
Rachel <rachel.jansen0613@gmail.com>
2021-07-17 15:15:51 -0500

    completed assignment

--- app_one/views.py
+++ app_one/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 from time import gmtime, strftime
-# from time import time, strftime
+# from datetime import datetime, timezone
 
+# current_date = datetime.now().date()
+# print(current_date)
+# utc_current_time = datetime.now(timezone.utc).time()
+# print(utc_current_time)
     
 def index(request):
     context = {
-        "time": strftime("%b %d, %I:%M %p", gmtime())
-        # "time": strftime("%b %d, %I:%M %p", time.localtime())
-
+        "time": strftime("%b %d, %Y %I:%M %p", gmtime())
     }
-    return render(request,'index.html', context)
+    return render(request, 'index.html', context)
 
+# def index(request):
+#     context = {
+#         "date": current_date,
+#         "time": utc_current_time
+#     }
+#     return render(request, 'index.html', context)
 
 # Create your views here.
 
